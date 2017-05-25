1a1c373a67abc740ff34b7dcc794c7f008f89421
JosephGarrone <josephgarrone@gmail.com>
2017-05-25 21:19:33 +1000

    Added webservice for confidence and current driver prediction. Number of sensors is now also returned via a webservice.

--- web-app/server.py
+++ web-app/server.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import random
+
 app = Flask(__name__, static_url_path='/public')
 
 @app.route("/server/get")
@@ -16,6 +18,17 @@ def js():
 @app.route("/third-party/vis.custom.js")
 def vis():
     return app.send_static_file('third-party/vis.custom.js')
+
+@app.route("/data/driver")
+def driver():
+    names = ["John", "Peter", "Paul", "Jonah", "Mitchell"]
+    name = names[random.randint(0, len(names) - 1)]
+    confidence = random.randint(0, 1000) / 10.0
+    return '{"name": "' + name + '", "confidence": "' + str(confidence) + '"}'
+
+@app.route("/data/sensor_count")
+def sensor_count():
+    return '{"count": "' + str(6) + '"}'
 
     
 if __name__ == "__main__":
--- web-app/static/js/main.js
+++ web-app/static/js/main.js
@@ -19,77 +19,116 @@ var cameraPositions = [
 var vectorGraphs = [];
 
 $(document).ready(function() {
-    for (var sensor = 1; sensor <= 3; sensor++) {
-        if (sensor > 1) {
-            $new = $("#sensor-1").clone()
-
-            $new.attr("id", "sensor-" + sensor);
-            $new.find(".sensor-name").first().html("Sensor " + sensor);
-            $new.find(".show-hide-toggle").first().attr("data-target", "#sensor-" + sensor + "-collapse");
-            $new.find(".collapse").first().attr("id", "sensor-" + sensor + "-collapse");
-            
-            $new.appendTo($("#sensor-1").parent());
-        }
-
-        $sensor = $("#sensor-" + sensor);
-
-        $dataX = $sensor.find(".data-x").first();
-        $dataY = $sensor.find(".data-y").first();
-        $dataZ = $sensor.find(".data-z").first();
-
-        chartElement = $sensor.find(".chart").first().get(0);
-        chartElement.setAttribute("width", $(chartElement.parentNode).width());
-
-        vectorElement = $sensor.find(".vector").first().get(0);
-
-        var x = new TimeSeries();
-        var y = new TimeSeries();
-        var z = new TimeSeries();
-
-        createVectorGraph(vectorElement, 1, 0, 0, sensor);
-
-        if ($(window).width() > 992) {
-            chartElement.setAttribute("height", $(chartElement.parentNode).height());
-        }
-
-        (function($dataX, $dataY, $dataZ, x, y, z, vectorElement, sensor) {
-            setInterval(function() {
-                var rx = Math.floor((Math.random() * 2) * 10000) / 10000;
-                var ry = Math.floor((Math.random() * 2) * 10000) / 10000;
-                var rz = Math.floor((Math.random() * 2) * 10000) / 10000;
-
-                x.append(new Date().getTime(), rx);
-                y.append(new Date().getTime(), ry);
-                z.append(new Date().getTime(), rz);
-
-                if (x.length > 50) {
-                    x.shift();
-                }
-                if (y.length > 50) {
-                    y.shift();
-                }
-                if (z.length > 50) {
-                    z.shift();
-                }
-
-                $dataX.html(rx);
-                $dataY.html(ry);
-                $dataZ.html(rz);
-
-                updateVectorGraph(vectorElement, rx, ry, rz, sensor);
-            }, 750);
-        })($dataX, $dataY, $dataZ, x, y, z, vectorElement, sensor);
-        
-        var chart = new SmoothieChart();
-        chart.addTimeSeries(x, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 3 });
-        chart.addTimeSeries(y, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 3 });
-        chart.addTimeSeries(z, { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.2)', lineWidth: 3 });
-        chart.streamTo(chartElement, 500);
-    }
+    setupSensors();
+    setupPrediction();
 })
 
+/**
+ * PREDICTION FUNCTIONS
+ */
+
+function setupPrediction() {
+    $driverName = $("#driver-name");
+    $driverConfidence = $("#driver-confidence");
+
+    function getDriverData() {
+        $.get({
+            url: "/data/driver",
+            dataType: "json"
+        }).done(function(data) {
+            console.log(data);
+            $driverName.html(data.name);
+            $driverConfidence.html(data.confidence + "%")
+        })
+
+        setTimeout(getDriverData, (Math.random() * 5 + 2) * 1000);
+    }
+
+    getDriverData();
+}
+
+
+/**
+ * SENSOR TAG FUNCTIONS
+ */
+
+function setupSensors() {
+    $.get({
+        url: "/data/sensor_count",
+        dataType: "json"
+    }).done(function(data) {
+        for (var sensor = 1; sensor <= data.count; sensor++) {
+            if (sensor > 1) {
+                $new = $("#sensor-1").clone()
+
+                $new.attr("id", "sensor-" + sensor);
+                $new.find(".sensor-name").first().html("Sensor " + sensor);
+                $new.find(".show-hide-toggle").first().attr("data-target", "#sensor-" + sensor + "-collapse");
+                $new.find(".collapse").first().attr("id", "sensor-" + sensor + "-collapse");
+                
+                $new.appendTo($("#sensor-1").parent());
+            }
+
+            $sensor = $("#sensor-" + sensor);
+
+            $dataX = $sensor.find(".data-x").first();
+            $dataY = $sensor.find(".data-y").first();
+            $dataZ = $sensor.find(".data-z").first();
+
+            chartElement = $sensor.find(".chart").first().get(0);
+            chartElement.setAttribute("width", $(chartElement.parentNode).width());
+
+            vectorElement = $sensor.find(".vector").first().get(0);
+
+            var x = new TimeSeries();
+            var y = new TimeSeries();
+            var z = new TimeSeries();
+
+            createVectorGraph(vectorElement, 1, 0, 0, sensor);
+
+            if ($(window).width() > 992) {
+                chartElement.setAttribute("height", $(chartElement.parentNode).height());
+            }
+
+            (function($dataX, $dataY, $dataZ, x, y, z, vectorElement, sensor) {
+                setInterval(function() {
+                    var rx = Math.floor((Math.random() * 2) * 10000) / 10000;
+                    var ry = Math.floor((Math.random() * 2) * 10000) / 10000;
+                    var rz = Math.floor((Math.random() * 2) * 10000) / 10000;
+
+                    x.append(new Date().getTime(), rx);
+                    y.append(new Date().getTime(), ry);
+                    z.append(new Date().getTime(), rz);
+
+                    if (x.length > 50) {
+                        x.shift();
+                    }
+                    if (y.length > 50) {
+                        y.shift();
+                    }
+                    if (z.length > 50) {
+                        z.shift();
+                    }
+
+                    $dataX.html(rx);
+                    $dataY.html(ry);
+                    $dataZ.html(rz);
+
+                    updateVectorGraph(vectorElement, rx, ry, rz, sensor);
+                }, 750);
+            })($dataX, $dataY, $dataZ, x, y, z, vectorElement, sensor);
+            
+            var chart = new SmoothieChart();
+            chart.addTimeSeries(x, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 3 });
+            chart.addTimeSeries(y, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 3 });
+            chart.addTimeSeries(z, { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.2)', lineWidth: 3 });
+            chart.streamTo(chartElement, 500);
+        }
+    })
+}
+
 function createVectorGraph(vectorElement, v1, v2, v3, sensor) {
-var min = 0.0;
+    var min = 0.0;
     var max = 2.0;
 
     options = {
